24c32b8b0ec2c87155750fba168e9abec563619c
ImSam_dev <imashansam@gmail.com>
2026-04-29 22:30:56 +0530

    Initial project setup with postgresql configuration

---
+++ src/main/java/com/fittrackpro/FitTrackProApplication.java
@@ -0,0 +1,13 @@
+package com.fittrackpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackProApplication.class, args);
+	}
+
+}
