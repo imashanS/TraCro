b0d4cfb98316311bdcf07de689e32bbddd94d709
ImSam_dev <imashansam@gmail.com>
2026-04-30 12:24:57 +0530

    Create repository

---
+++ src/main/java/com/fittrackpro/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fittrackpro.repository;
+
+import com.fittrackpro.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}gi
