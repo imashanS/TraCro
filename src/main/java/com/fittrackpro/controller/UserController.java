a89c2fe2190ffc44194e50277f7e5451c34b5ed1
ImSam_dev <imashansam@gmail.com>
2026-05-03 18:11:54 +0530

    add user service and basic CRUD endpoints

---
+++ src/main/java/com/fittrackpro/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.fittrackpro.controller;
+
+import com.fittrackpro.entity.User;
+import com.fittrackpro.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
