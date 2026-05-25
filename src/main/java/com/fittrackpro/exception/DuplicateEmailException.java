78848e5c129a4d40e5df353361dfe50e7b9eab29
ImSam_dev <imashansam@gmail.com>
2026-05-25 14:22:05 +0530

    fix: restructure into backend/ and frontend/ folders

--- src/main/java/com/fittrackpro/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.fittrackpro.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
